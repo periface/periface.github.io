45e5fe536b825a32f290e4c7a253984ab325edea
Alan Torres <temp_peri@hotmail.com>
2018-03-21 16:43:48 -0600

    update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
-﻿(function(){
-    
-    var loadContributions = function(){
-        $.get("https://api.github.com/repos/CodefyMX/NorthLionAbpZero/stats/code_frequency",function(data){
-            console.log(data);
-        });
-    }
+﻿(function() {
+  var loadContributions = function() {
+    $.get(
+      "https://api.github.com/repos/CodefyMX/NorthLionAbpZero/stats/code_frequency",
+      function(data) {
+        console.log(data);
+      }
+    );
+  };
 
-    loadContributions();
-    
+  loadContributions();
+  var typed = new Typed("#text", {
+    strings: [
+      "Runner",
+      "Amante de la pizza",
+      ""
+    ],
+    typeSpeed: 100,
+    backDelay: 1000,
+    startDelay: 1000,
+    loop: true,
+    fadeOut: true,
+  });
 })();
