5dc493d6f39a73349bcf74589a9b37cbfc15edc7
Alan Torres <temp_peri@hotmail.com>
2018-03-21 16:08:56 -0600

    Portfolio update

---
+++ js/index.js
@@ -0,0 +1,11 @@
+﻿(function(){
+    
+    var loadContributions = function(){
+        $.get("https://api.github.com/repos/CodefyMX/NorthLionAbpZero/stats/code_frequency",function(data){
+            console.log(data);
+        });
+    }
+
+    loadContributions();
+    
+})();
