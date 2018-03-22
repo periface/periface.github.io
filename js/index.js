8e7d04bf494df563b01fde9c523494729a16ffd4
Alan Torres <temp_peri@hotmail.com>
2018-03-22 15:28:08 -0600

    cv update

--- js/index.js
+++ js/index.js
@@ -10,15 +10,45 @@
 
   loadContributions();
   var typed = new Typed("#text", {
-    strings: [
-      "Runner",
-      "Amante de la pizza",
-      ""
-    ],
+    strings: ["Runner", "Amante de la pizza", ""],
     typeSpeed: 100,
     backDelay: 1000,
     startDelay: 1000,
     loop: true,
-    fadeOut: true,
+    fadeOut: true
+  });
+  var waypoint = new Waypoint({
+    element: $(".header-limit"),
+    handler: function(direction) {
+      if (direction == "down") {
+        $(".menu-list").addClass("fixed-top menu-fixed");
+        $(".menu-photo").fadeIn();
+      } else {
+        $(".menu-list").removeClass("fixed-top menu-fixed");
+        $(".menu-photo").fadeOut();
+      }
+    }
+  });
+
+  var waypoint = new Waypoint({
+    element: $(".videogamer"),
+    handler: function(direction) {
+      if (direction == "down") {
+        $(".videogamer img").addClass("fixed-top");
+      } else {
+        $(".videogamer img").removeClass("fixed-top");
+        $(".videogamer img").removeClass("fade-out");
+        $(".videogamer img").removeClass("fade-in");
+      }
+    },
+    offset: 30
+  });
+
+  var waypoint = new Waypoint({
+    element: $("#portfolio"),
+    handler: function(direction) {
+      $(".videogamer img").addClass("fade-out");
+      $(".videogamer img").removeClass("fade-in");
+    }
   });
 })();
