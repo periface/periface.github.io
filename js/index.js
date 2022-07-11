8833427d4fc082630892b3a304fcf374b76c8af2
Alan Torres <temp_peri@hotmail.com>
2022-07-11 14:02:06 -0500

    conshita

--- js/index.js
+++ js/index.js
@@ -6,10 +6,12 @@ const {
   Runner,
   Bodies,
   Composite,
+  Composites,
   Mouse,
   World,
   MouseConstraint,
   Constraint,
+  Body,
 } = Matter;
 let engine;
 let render;
@@ -20,74 +22,136 @@ let cssBox;
 let htmlBox;
 let husky;
 let ground;
+let table;
+let coffe;
+let images = [
+  {
+    img: "../img/red-bull.png",
+    scale: {
+      xScale: 0.04,
+      yScale: 0.04,
+    },
+  },
+  {
+    img: "../img/book.png",
+    scale: {
+      xScale: 0.02,
+      yScale: 0.02,
+    },
+  },
+];
+
+/**
+ *   "../img/js.png",
+  "../img/html.png",
+  "../img/css.png",
+ */
+
+let pc;
 function init() {
   setup();
+  table = new Rectangle({
+    x: canvas.width - 300,
+    y: canvas.height - 100,
+    w: 400,
+    h: 175,
+    isStatic: true,
+    bodyRestitution: 0,
+    options: {
+      render: {
+        sprite: {
+          texture: "../img/table2.png",
+          xScale: 0.8,
+          yScale: 0.9,
+        },
+      },
+    },
+  });
+  pc = new Rectangle({
+    x: table.body.position.x,
+    y: canvas.height - 150,
+    w: 150,
+    h: 130,
+    isStatic: false,
+    bodyRestitution: 0,
+    options: {
+      render: {
+        sprite: {
+          texture: "../img/pc.png",
+          xScale: 0.5,
+          yScale: 0.5,
+        },
+      },
+    },
+  });
+  coffe = new Rectangle({
+    x: table.body.position.x - 100,
+    y: table.body.position.y - table.h,
+    w: 30,
+    h: 30,
+    bodyRestitution: 0,
+    options: {
+      render: {
+        sprite: {
+          texture: "../img/coffe.png",
+          xScale: 0.05,
+          yScale: 0.05,
+        },
+      },
+    },
+  });
 
-  jsBox = new Rectangle({
-    x: canvas.width - 200,
-    y: 0,
-    w: 100,
-    h: 115,
-    bodyRestitution: 0,
-    options: {
-      render: {
-        sprite: {
-          texture: "../img/js.png",
-          xScale: 0.3,
-          yScale: 0.3,
+  showObjects();
+  Render.run(render);
+  startLoop();
+}
+function showObjects() {
+  var stack = Composites.stack(
+    pc.body.position.x,
+    pc.body.position.y - pc.h,
+    2,
+    2,
+    15,
+    15,
+    function (x, y) {
+      var image = images[Math.floor(Math.random() * images.length)];
+
+      return new Rectangle({
+        x: x,
+        y: y,
+        w: 50,
+        h: 50,
+        options: {
+          render: {
+            sprite: {
+              texture: image.img,
+              xScale: image.scale.xScale,
+              yScale: image.scale.yScale,
+            },
+          },
         },
-      },
-    },
-  });
-  cssBox = new Rectangle({
-    x: canvas.width - 200,
-    y: 500,
-    w: 100,
-    h: 200,
-    bodyRestitution: 0,
-    options: {
-      render: {
-        sprite: {
-          texture: "../img/css.png",
-          xScale: 0.3,
-          yScale: 0.3,
-        },
-      },
-    },
-  });
-  htmlBox = new Rectangle({
-    x: canvas.width - 200,
-    y: -300,
-    w: 100,
-    h: 200,
-    bodyRestitution: 0,
-    options: {
-      render: {
-        sprite: {
-          texture: "../img/html.png",
-          xScale: 0.3,
-          yScale: 0.3,
-        },
-      },
-    },
-  });
+      }).body;
+    }
+  );
   ground = new Rectangle({
     x: 0,
     y: canvas.height,
     w: canvas.width * 2,
-    h: 50,
+    h: 20,
     options: {
-      render: {
-        fillStyle: "transparent",
-      },
+      // render: {
+      //   fillStyle: "transparent",
+      // },
     },
     isStatic: true,
   });
 
-  husky = new Circle({
+  husky = new Rectangle({
     x: -canvas.width,
-    y: canvas.height - 200,
-    r: 200,
+    y: canvas.height - 100,
+    w: 300,
+    h: 200,
+    bodyRestitution: 0.5,
     options: {
       render: {
         fillStyle: "transparent",
@@ -99,21 +163,46 @@ function init() {
       scale: 4,
       framesHold: 5,
       offset: {
-        x: 0,
-        y: 0,
+        x: 150,
+        y: 60,
       },
     },
     isStatic: false,
   });
 
-  Render.run(render);
-  startLoop();
+  // Body.applyForce(
+  //   husky.body,
+  //   { x: husky.body.position.x, y: husky.body.position.y },
+  //   { x: 5, y: 0 }
+  // );
 }
+let force = false;
 function startLoop() {
   requestAnimationFrame(startLoop);
   Engine.update(engine);
   husky.update();
-  husky.body.position.x += 0.3;
+  // Body.applyForce(
+  //   husky.body,
+  //   { x: husky.body.position.x, y: husky.body.position.y },
+  //   { x: 0.05, y: 0 }
+  // );
+  husky.body.friction = 0;
+  husky.body.mass = 30;
+
+  if (husky.body.position.x > canvas.width / 2 - 100 && !force) {
+    Body.applyForce(
+      husky.body,
+      { x: husky.body.position.x, y: husky.body.position.y },
+      { x: 0, y: -1.5 }
+    );
+    force = true;
+  } else {
+    husky.body.position.x += 0.15;
+  }
+  if (husky.body.position.x > canvas.width + 1000) {
+    showObjects();
+    force = false;
+  }
 }
 function setup() {
   engine = Engine.create();
@@ -131,4 +220,5 @@ function setup() {
   });
   world = engine.world;
 }
+
 init();
--- js/objects/rectangle.js
+++ js/objects/rectangle.js
@@ -7,13 +7,27 @@ class Rectangle {
     isStatic = false,
     disablePhysics = false,
     bodyRestitution = 0.5,
+    autoAdd = true,
+    animationOptions,
     options = {},
   }) {
     this.body = Matter.Bodies.rectangle(x, y, w, h, options);
-    Matter.World.add(world, this.body);
+    if (autoAdd) {
+      Matter.World.add(world, this.body);
+    }
     this.body.restitution = bodyRestitution;
     this.w = w;
     this.h = h;
+
+    if (animationOptions) {
+      this.imageSrc = animationOptions.imageSrc;
+      this.framesMax = animationOptions.framesMax;
+      this.scale = animationOptions.scale || 1;
+      this.framesCurrent = 0;
+      this.framesElapsed = 0;
+      this.framesHold = animationOptions.framesHold || 5;
+      this.offset = animationOptions.offset || { x: 0, y: 0 };
+    }
     if (isStatic) {
       Matter.Body.setStatic(this.body, true);
     }
@@ -24,6 +38,52 @@ class Rectangle {
         mask: 0,
       };
     }
+    if (animationOptions && animationOptions.imageSrc) {
+      try {
+        this.image = new Image();
+        this.image.src = this.imageSrc;
+        this.image.onload = () => {
+          this.imageLoaded = true;
+        };
+      } catch (error) {
+        console.log("no se pudo construir la imagen");
+      }
+    }
+  }
+  animateFrames() {
+    this.framesElapsed++;
+    if (this.framesElapsed % this.framesHold === 0) {
+      if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++;
+      } else {
+        this.animationEnded = true;
+        this.framesCurrent = 0;
+      }
+    }
+  }
+  update() {
+    try {
+      if (this.imageLoaded) {
+        this.drawImage();
+        this.animateFrames();
+      }
+      //Matter.Engine.update(engine); //INNECESARIO
+    } catch (error) {
+      console.warn("No hay animaciones definidas", error);
+    }
+  }
+  drawImage() {
+    ctx.drawImage(
+      this.image,
+      this.framesCurrent * (this.image.width / this.framesMax),
+      0,
+      this.image.width / this.framesMax,
+      this.image.height,
+      this.body.position.x - this.offset.x,
+      this.body.position.y - this.offset.y,
+      (this.image.width / this.framesMax) * this.scale,
+      this.image.height * this.scale
+    );
   }
   destroy() {
     Matter.World.remove(world, this);
