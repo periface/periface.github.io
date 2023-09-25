004f62f737193741dfd32f346e9aa11242d7952b
Alan Torres <temp_peri@hotmail.com>
2023-09-25 13:21:08 -0600

    formulario aptra con mocks

--- formulario_aptra/index.js
+++ formulario_aptra/index.js
@@ -1,54 +1,144 @@
+let loading = false; // NI SE USA 🤡
+let contextoModal = 0; // NO QUERIA REPETIR CODIGO, PERO NO SE ME OCURRIO OTRA FORMA DE HACERLO EJEMPLO: 0 = ejemploFotoModal, 1 = subirFotoModal
 
-function swap(val1, val2) {
-    let temp = val1;
-    val1 = val2;
-    val2 = temp;
-}
-function bubbleSort() {
-    const toSort = [4, 2, 1, 3];
-    console.log('Bubble Sort');
-    console.log(toSort);
-    let i, j;
-    let swapped;
-    for (i = 0; i < toSort.length; i++) {
-        swapped = false;
-        for (j = 0; j < toSort.length - i - 1; j++) {
-            if (toSort[j] > toSort[j + 1]) {
-                swap(toSort[j], toSort[j + 1]);
-                swapped = true;
+const ejemploFotoBTN = document.querySelector('#ejemploFotoBtn');
+const startOverBTN = document.querySelector('#startOver');
+const siBTN = document.querySelector('#si');
+const noBTN = document.querySelector('#no');
 
-            }
-        }
-        if (swapped === false) {
-            break;
-        }
+const ejemploFotoIMG = document.querySelector('#ejemploFotoImg');
+const successIMG = document.querySelector('#successimg');
+
+const exampleFotoModal = new bootstrap.Modal(document.querySelector('#exampleFoto'));
+const previousFotoModal = new bootstrap.Modal(document.querySelector('#previousFoto'));
+const subirFotoModal = new bootstrap.Modal(document.querySelector('#subirFoto'));
+
+const successImgContainerDIV = document.querySelector('#successImgContainer');
+const successImgResultDIV = document.querySelector('#successImgResult');
+const fotoMainDIV = document.querySelector('#fotoMain');
+const fotoMainFinishedDIV = document.querySelector('#fotoMainFinished');
+const subirFotoContainerDIV = document.querySelector('#subirFotoContainer');
+const subirFotoContainerLoadingDIV = document.querySelector('#subirFotoContainerLoading');
+
+const successGifRoute = './img/success.gif';
+const upFotoINPUT = document.querySelector('#up-foto');
+
+
+
+siBTN.addEventListener('click', () => {
+    if (contextoModal === 0) {
+        exampleFotoModal.hide();
+        fotoMainDIV.classList.add('d-none');
+        fotoMainFinishedDIV.style.display = 'block';
+        previousFotoModal.hide();
     }
-    console.log(toSort);
-}
-
-function cantBelieveItWorks() {
-    const toSort = [4, 2, 1, 3];
-    console.log('Cant Believe It Works');
-    console.log(toSort);
-    for (let i = 0; i < toSort.length; i++) {
-        let iElement = toSort[i];
-        for (let j = 0; j < toSort.length; j++) {
-            let jElement = toSort[j];
-            if (iElement > jElement) {
-                // let temp = iElement;
-                // iElement = jElement;
-                // jElement = temp;
-                // toSort[i] = iElement;
-                // toSort[j] = jElement;
-                [toSort[i], toSort[j]] = [toSort[j], toSort[i]];
-            }
-        }
+    else {
+        previousFotoModal.hide();
+        fotoMainDIV.classList.add('d-none');
+        fotoMainFinishedDIV.style.display = 'block';
     }
-    console.log(toSort);
+});
+
+noBTN.addEventListener('click', () => {
+    if (contextoModal === 0) {
+        exampleFotoModal.hide();
+        previousFotoModal.show();
+    }
+    else {
+        previousFotoModal.hide();
+        subirFotoModal.show();
+    }
+});
+
+startOverBTN.addEventListener('click', () => {
+    fotoMainDIV.classList.remove('d-none');
+    fotoMainFinishedDIV.style.display = 'none';
+});
+
+async function onFileChange(evt) {
+    try {
+        await uploadFotoProcess();
+    } catch (error) {
+        console.log(error);
+        resetWhenError();
+    }
 }
 
-function runAll() {
-    bubbleSort();
-    cantBelieveItWorks();
+ejemploFotoBTN.addEventListener('click', async () => {
+    try {
+        await takeFotoProcess();
+    }
+    catch (error) {
+        console.log(error);
+        resetWhenError();
+    }
+
+});
+$(document).ready(function () {
+    $('.select2me').select2({
+        theme: "bootstrap-5",
+        width: '100%',
+    });
+});
+
+function resetWhenError() {
+    // generado por IA 🤡 no creo que jale, pero por si las dudas
+    ejemploFotoIMG.src = './ejemplo.png';
+    ejemploFotoBTN.disabled = false;
+    subirFotoContainerDIV.style.display = 'block';
+    subirFotoContainerLoadingDIV.style.display = 'none';
+    upFotoINPUT.value = '';
+    successImgContainerDIV.style.display = 'block';
+    successImgResultDIV.style.display = 'none';
+    fotoMainDIV.classList.remove('d-none');
+    fotoMainFinishedDIV.style.display = 'none';
+    exampleFotoModal.hide();
+    previousFotoModal.hide();
+    subirFotoModal.hide();
 }
-runAll();
+
+function fakeAsync() {
+    loading = true;
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            loading = false;
+            resolve();
+        }, 3000);
+    });
+}
+
+async function takeFotoProcess() {
+    contextoModal = 0;
+    ejemploFotoIMG.src = './img/loading.gif';
+    // disable button
+    ejemploFotoBTN.disabled = true;
+    await fakeAsync();
+    ejemploFotoIMG.src = './ejemplo.png';
+    // enable button
+    ejemploFotoBTN.disabled = false;
+    exampleFotoModal.hide();
+    successIMG.src = successGifRoute;
+    previousFotoModal.show();
+
+    setTimeout(() => {
+        successImgContainerDIV.style.display = 'none';
+        successImgResultDIV.style.display = 'block';
+    }, 5000);
+}
+
+async function uploadFotoProcess() {
+    contextoModal = 1;
+    subirFotoContainerDIV.style.display = 'none';
+    subirFotoContainerLoadingDIV.style.display = 'block';
+    await fakeAsync();
+    subirFotoContainerDIV.style.display = 'block';
+    subirFotoContainerLoadingDIV.style.display = 'none';
+    upFotoINPUT.value = '';
+    subirFotoModal.hide();
+    previousFotoModal.show();
+    successIMG.src = successGifRoute;
+    setTimeout(() => {
+        successImgContainerDIV.style.display = 'none';
+        successImgResultDIV.style.display = 'block';
+    }, 5000);
+}
